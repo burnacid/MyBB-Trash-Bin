d5480463244c77bc3ff0a2591f025aee2efcf0a3
Stefan Lenders <stefan@lenders-it.nl>
2017-03-08 19:06:12 +0100

    Added initial files

---
+++ admin/modules/tools/trashbin.php
@@ -0,0 +1,193 @@
+<?php
+
+$trashbin = new trashbin;
+
+$sub_tabs['trashbin_threads'] = array(
+    'title' => 'Thread Trash Bin',
+    'link' => 'index.php?module=tools-trashbin',
+    'description' => 'View all removed threads.');
+$sub_tabs['trashbin_posts'] = array(
+    'title' => 'Post Trash Bin',
+    'link' => 'index.php?module=tools-trashbin&amp;action=posts',
+    'description' => 'View all removed posts.');
+
+switch ($mybb->get_input('action'))
+{
+    case 'threadrestore':
+        $sub_tabs['trashbin_threadrestore'] = array(
+            'title' => "Restore thread",
+            'link' => 'index.php?module=tools-trashbin&amp;action=threadrestore&amp;tid=' . $mybb->input['tid'],
+            'description' => "");
+        break;
+    case 'postrestore':
+        $sub_tabs['trashbin_postrestore'] = array(
+            'title' => "Restore post",
+            'link' => 'index.php?module=tools-trashbin&amp;action=postrestore&amp;pid=' . $mybb->input['pid'],
+            'description' => "");
+        break;
+}
+
+if ($mybb->get_input('action') == 'posts')
+{
+    $page->add_breadcrumb_item("Trash Bin", "");
+    $page->output_header("Trash Bin");
+    $page->output_nav_tabs($sub_tabs, 'trashbin_posts');
+
+    $table = new Table;
+    $table->construct_header("Thread subject", array());
+    $table->construct_header("Post subject", array());
+    $table->construct_header("Poster", array());
+    $table->construct_header("Deleted by", array());
+    $table->construct_header("Deleted on", array());
+    $table->construct_header("", array());
+
+
+    $query = $db->simple_select('trashbin_posts_single', '*', '', array("order_by" => "deletetime"));
+
+    if (!$db->num_rows($query))
+    {
+        $table->construct_cell('<div align="center">The trash bin is empty</div>', array('colspan' => 6));
+        $table->construct_row();
+        $table->output("Post Trash Bin");
+    }
+    else
+    {
+        while ($post = $db->fetch_array($query))
+        {
+            $restore_link = "index.php?module=tools-trashbin&amp;action=postrestore&amp;pid={$post['pid']}";
+
+            $thread = get_thread($post['tid']);
+            if ($thread)
+            {
+                $table->construct_cell("<a href='../showthread.php?tid=" . $thread['tid'] . "'>" . $thread['subject'] . "</a>");
+            }
+            else
+            {
+                $table->construct_cell("- REMOVED THREAD -");
+            }
+
+            $table->construct_cell($post['subject']);
+
+            //poster
+            $poster = get_user($post['uid']);
+            $table->construct_cell("<a href='../member.php?uid=" . $poster['uid'] . "'>" . $poster['username'] . "</a>");
+
+            //deleter
+            $deletedby = get_user($post['deletedby']);
+            $table->construct_cell("<a href='../member.php?uid=" . $deletedby['uid'] . "'>" . $deletedby['username'] . "</a>");
+
+            $table->construct_cell(date("d-m-Y H:i", $post['deletetime']));
+
+            $popup = new PopupMenu("post_{$post['pid']}", $lang->options);
+            $popup->add_item("Restore", $restore_link);
+
+            if ($thread)
+            {
+                $table->construct_cell($popup->fetch(), array('class' => 'align_center'));
+            }
+            else
+            {
+                $table->construct_cell("", array('class' => 'align_center'));
+            }
+
+            $table->construct_row();
+        }
+        $table->output("Post Trash Bin");
+
+        $form->end();
+    }
+
+    $page->output_footer();
+}
+elseif ($mybb->get_input('action') == 'threadrestore')
+{
+    if ($mybb->input['tid'])
+    {
+        trashbin_restore_thread($mybb->input['tid']);
+
+        $trashbin->admin_redirect("The selected thread has been restored.");
+    }
+    else
+    {
+        $trashbin->admin_redirect();
+    }
+}
+elseif ($mybb->get_input('action') == 'postrestore')
+{
+    if ($mybb->input['pid'])
+    {
+        $result = trashbin_restore_post($mybb->input['pid']);
+    
+        if($result[0]){
+            $trashbin->admin_redirect("The selected post has been restored.", false, "posts");
+        }else{
+            $trashbin->admin_redirect($result[1], true, "posts");
+        }
+    }
+    else
+    {
+        $trashbin->admin_redirect("Oops something went wrong", true, "posts");
+    }
+}
+else
+{
+
+    $page->add_breadcrumb_item("Trash Bin", "");
+    $page->output_header("Trash Bin");
+    $page->output_nav_tabs($sub_tabs, 'trashbin_threads');
+
+    $table = new Table;
+    $table->construct_header("Thread subject", array());
+    $table->construct_header("Poster", array());
+    $table->construct_header("Posts", array());
+    $table->construct_header("Deleted by", array());
+    $table->construct_header("Deleted on", array());
+    $table->construct_header("", array());
+
+
+    $query = $db->simple_select('trashbin_threads', '*', '', array("order_by" => "deletetime"));
+
+    if (!$db->num_rows($query))
+    {
+        $table->construct_cell('<div align="center">The trash bin is empty</div>', array('colspan' => 6));
+        $table->construct_row();
+        $table->output("Threads Trash Bin");
+    }
+    else
+    {
+        while ($thread = $db->fetch_array($query))
+        {
+            $restore_link = "index.php?module=tools-trashbin&amp;action=threadrestore&amp;tid={$thread['tid']}";
+
+            $table->construct_cell($thread['subject']);
+
+            //poster
+            $poster = get_user($thread['uid']);
+            $table->construct_cell("<a href='../member.php?uid=" . $poster['uid'] . "'>" . $poster['username'] . "</a>");
+
+            //num posts
+            $query2 = $db->simple_select("trashbin_posts", "pid", "tid = " . $thread['tid']);
+            $table->construct_cell($db->num_rows($query2));
+
+            //deleter
+            $deletedby = get_user($thread['deletedby']);
+            $table->construct_cell("<a href='../member.php?uid=" . $deletedby['uid'] . "'>" . $deletedby['username'] . "</a>");
+
+            $table->construct_cell(date("d-m-Y H:i", $thread['deletetime']));
+
+            $popup = new PopupMenu("thread_{$thread['tid']}", $lang->options);
+            $popup->add_item("Restore", $restore_link);
+
+            $table->construct_cell($popup->fetch(), array('class' => 'align_center'));
+
+            $table->construct_row();
+        }
+        $table->output("Threads Trash Bin");
+
+        $form->end();
+    }
+
+    $page->output_footer();
+}
+
+?>
---
+++ inc/plugins/trashbin.php
@@ -0,0 +1,367 @@
+<?php
+
+if (!defined('IN_MYBB'))
+    die('This file cannot be accessed directly.');
+
+//HOOKS
+if (defined('IN_ADMINCP'))
+{
+    $plugins->add_hook('admin_tools_menu', create_function('&$args', '$args[] = array(\'id\' => \'trashbin\', \'title\' => \'Trash Bin\', \'link\' => \'index.php?module=tools-trashbin\');'));
+    $plugins->add_hook('admin_tools_action_handler', create_function('&$args', '$args[\'trashbin\'] = array(\'active\' => \'trashbin\', \'file\' => \'trashbin.php\');'));
+}
+else
+{
+    $plugins->add_hook("class_moderation_delete_thread_start", "trashbin_delete_thread");
+    $plugins->add_hook("class_moderation_delete_post_start", "trashbin_delete_post");
+}
+
+function trashbin_info()
+{
+    return array(
+        'name' => 'Trash Bin',
+        'description' => 'Moves all permanently deleted threads to a trashbin',
+        'website' => 'http://lenders-it.nl',
+        'author' => 'S. Lenders',
+        'authorsite' => 'http://lenders-it.nl',
+        'version' => '0.1',
+        'compatibility' => '18*',
+        'guid' => '');
+}
+
+function trashbin_install()
+{
+    global $db, $mybb;
+
+
+    // Create our table collation
+    $collation = $db->build_create_table_collation();
+
+    // Create table if it doesn't exist already
+    if (!$db->table_exists('trashbin_posts'))
+    {
+        $db->write_query("CREATE TABLE IF NOT EXISTS `" . TABLE_PREFIX . "trashbin_posts` (
+          `pid` int(10) unsigned NOT NULL,
+          `tid` int(10) unsigned NOT NULL DEFAULT '0',
+          `replyto` int(10) unsigned NOT NULL DEFAULT '0',
+          `fid` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `subject` varchar(120) NOT NULL DEFAULT '',
+          `icon` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `uid` int(10) unsigned NOT NULL DEFAULT '0',
+          `username` varchar(80) NOT NULL DEFAULT '',
+          `dateline` int(10) unsigned NOT NULL DEFAULT '0',
+          `message` text NOT NULL,
+          `ipaddress` varbinary(16) NOT NULL DEFAULT '',
+          `includesig` tinyint(1) NOT NULL DEFAULT '0',
+          `smilieoff` tinyint(1) NOT NULL DEFAULT '0',
+          `edituid` int(10) unsigned NOT NULL DEFAULT '0',
+          `edittime` int(10) unsigned NOT NULL DEFAULT '0',
+          `editreason` varchar(150) NOT NULL DEFAULT '',
+          `visible` tinyint(1) NOT NULL DEFAULT '0',
+          `tags` text NOT NULL,
+          PRIMARY KEY (`pid`),
+          KEY `tid` (`tid`,`uid`),
+          KEY `uid` (`uid`),
+          KEY `visible` (`visible`),
+          KEY `dateline` (`dateline`),
+          KEY `ipaddress` (`ipaddress`),
+          KEY `tiddate` (`tid`,`dateline`),
+          FULLTEXT KEY `message` (`message`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+
+    }
+
+    if (!$db->table_exists('trashbin_threads'))
+    {
+        $db->write_query("CREATE TABLE IF NOT EXISTS `" . TABLE_PREFIX . "trashbin_threads` (
+          `tid` int(10) unsigned NOT NULL,
+          `fid` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `subject` varchar(120) NOT NULL DEFAULT '',
+          `prefix` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `icon` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `poll` int(10) unsigned NOT NULL DEFAULT '0',
+          `uid` int(10) unsigned NOT NULL DEFAULT '0',
+          `username` varchar(80) NOT NULL DEFAULT '',
+          `dateline` int(10) unsigned NOT NULL DEFAULT '0',
+          `firstpost` int(10) unsigned NOT NULL DEFAULT '0',
+          `lastpost` int(10) unsigned NOT NULL DEFAULT '0',
+          `lastposter` varchar(120) NOT NULL DEFAULT '',
+          `lastposteruid` int(10) unsigned NOT NULL DEFAULT '0',
+          `views` int(100) unsigned NOT NULL DEFAULT '0',
+          `replies` int(100) unsigned NOT NULL DEFAULT '0',
+          `closed` varchar(30) NOT NULL DEFAULT '',
+          `sticky` tinyint(1) NOT NULL DEFAULT '0',
+          `numratings` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `totalratings` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `notes` text NOT NULL,
+          `visible` tinyint(1) NOT NULL DEFAULT '0',
+          `unapprovedposts` int(10) unsigned NOT NULL DEFAULT '0',
+          `deletedposts` int(10) unsigned NOT NULL DEFAULT '0',
+          `attachmentcount` int(10) unsigned NOT NULL DEFAULT '0',
+          `deletetime` int(10) unsigned NOT NULL DEFAULT '0',
+          `deletedby` int(10) unsigned NOT NULL DEFAULT '0',
+          PRIMARY KEY (`tid`),
+          KEY `fid` (`fid`,`visible`,`sticky`),
+          KEY `dateline` (`dateline`),
+          KEY `lastpost` (`lastpost`,`fid`),
+          KEY `firstpost` (`firstpost`),
+          KEY `uid` (`uid`),
+          FULLTEXT KEY `subject` (`subject`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8;");
+
+    }
+    
+    if (!$db->table_exists('trashbin_posts_single'))
+    {
+        $db->write_query("CREATE TABLE IF NOT EXISTS `".TABLE_PREFIX."trashbin_posts_single` (
+          `pid` int(10) unsigned NOT NULL,
+          `tid` int(10) unsigned NOT NULL DEFAULT '0',
+          `replyto` int(10) unsigned NOT NULL DEFAULT '0',
+          `fid` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `subject` varchar(120) NOT NULL DEFAULT '',
+          `icon` smallint(5) unsigned NOT NULL DEFAULT '0',
+          `uid` int(10) unsigned NOT NULL DEFAULT '0',
+          `username` varchar(80) NOT NULL DEFAULT '',
+          `dateline` int(10) unsigned NOT NULL DEFAULT '0',
+          `message` text NOT NULL,
+          `ipaddress` varbinary(16) NOT NULL DEFAULT '',
+          `includesig` tinyint(1) NOT NULL DEFAULT '0',
+          `smilieoff` tinyint(1) NOT NULL DEFAULT '0',
+          `edituid` int(10) unsigned NOT NULL DEFAULT '0',
+          `edittime` int(10) unsigned NOT NULL DEFAULT '0',
+          `editreason` varchar(150) NOT NULL DEFAULT '',
+          `visible` tinyint(1) NOT NULL DEFAULT '0',
+          `tags` text NOT NULL,
+          `deletetime` int(10) unsigned NOT NULL DEFAULT '0',
+          `deletedby` int(10) unsigned NOT NULL DEFAULT '0',
+          PRIMARY KEY (`pid`),
+          KEY `tid` (`tid`,`uid`),
+          KEY `uid` (`uid`),
+          KEY `visible` (`visible`),
+          KEY `dateline` (`dateline`),
+          KEY `ipaddress` (`ipaddress`),
+          KEY `tiddate` (`tid`,`dateline`),
+          FULLTEXT KEY `message` (`message`)
+        ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+        ");
+
+    }
+    
+}
+function trashbin_activate()
+{
+    change_admin_permission('tools', 'trashbin', 1);
+}
+function trashbin_is_installed()
+{
+    global $db;
+
+    // If the table exists then it means the plugin is installed because we only drop it on uninstallation
+    return $db->table_exists('trashbin_threads') && $db->table_exists('trashbin_posts');
+}
+function trashbin_deactivate()
+{
+    change_admin_permission('tools', 'trashbin', -1);
+}
+function trashbin_uninstall()
+{
+    global $db, $mybb;
+
+    if ($mybb->request_method != 'post')
+    {
+        global $page;
+
+        $page->output_confirm_action('index.php?module=config-plugins&action=deactivate&uninstall=1&plugin=trashbin',
+            "Are you sure you want to uninstall the Trash bin plugin? This will delete the complete contents of the trashbin.", "Uninstall Trashbin plugin");
+    }
+
+    // This is required so it updates the settings.php file as well and not only the database - they must be synchronized!
+    rebuild_settings();
+
+    // Drop tables if desired
+    if (!isset($mybb->input['no']))
+    {
+        $db->drop_table('trashbin_threads');
+        $db->drop_table('trashbin_posts');
+        $db->drop_table('trashbin_posts_single');
+    }
+}
+
+function trashbin_delete_post($pid)
+{
+    global $db, $mybb;
+
+    $query = $db->simple_select("posts", "*", "pid = " . intval($pid));
+
+    if ($db->num_rows($query) == 1)
+    {
+        $post = $db->fetch_array($query);
+        $post = trashbin_escape_post($post);
+        
+        $post['deletetime'] = time();
+        $post['deletedby'] = $mybb->user['uid'];
+
+        $db->insert_query("trashbin_posts_single", $post);
+    }
+}
+
+function trashbin_restore_post($pid)
+{
+    global $db, $mybb;
+
+    $query = $db->simple_select("trashbin_posts_single", "*", "pid = " . intval($pid));
+
+    if ($db->num_rows($query) == 1)
+    {
+        $post = $db->fetch_array($query);       
+        $thread = get_thread($post['tid']);
+        
+        if($thread){
+            $post = trashbin_escape_post($post);
+        
+            unset($post['deletetime']);
+            unset($post['deletedby']);
+    
+            $db->insert_query("posts", $post);
+            
+            $db->delete_query("trashbin_posts_single","pid = ". intval($pid));
+            
+            return array(true);
+        }else{
+            return array(false,"Thread does not longer exist!");
+        }
+        
+    }else{
+        return array(false,"The post your are trying to restore is not found!");
+    }
+}
+
+function trashbin_delete_thread($tid)
+{
+    global $db, $mybb;
+
+    $query = $db->simple_select("threads", "*", "tid = " . intval($tid));
+
+    if ($db->num_rows($query) == 1)
+    {
+        $thread = $db->fetch_array($query);
+        $thread['deletetime'] = time();
+        $thread['deletedby'] = $mybb->user['uid'];
+
+        $query2 = $db->simple_select("posts", "*", "tid = " . intval($tid));
+
+        while ($post = $db->fetch_array($query2))
+        {
+            $post = trashbin_escape_post($post);
+            $db->insert_query("trashbin_posts", $post);
+        }
+
+        $db->insert_query("trashbin_threads", $thread);
+    }
+}
+
+function trashbin_restore_thread($tid)
+{
+    global $db, $mybb;
+
+    $query = $db->simple_select("trashbin_threads", "*", "tid = " . intval($tid));
+
+    if ($db->num_rows($query) == 1)
+    {
+        $thread = $db->fetch_array($query);
+        $thread['deletetime'] = 0;
+        unset($thread['deletedby']);
+
+        $query2 = $db->simple_select("trashbin_posts", "*", "tid = " . intval($tid));
+
+        while ($post = $db->fetch_array($query2))
+        {
+            $post = trashbin_escape_post($post);
+            $db->insert_query("posts", $post);
+        }
+
+        $db->insert_query("threads", $thread);
+        
+        $db->delete_query("trashbin_posts","tid = ". intval($tid));
+        $db->delete_query("trashbin_threads","tid = ". intval($tid));
+    }
+}
+
+function trashbin_escape_post($post){
+    global $db;
+    
+    $post['subject'] = $db->escape_string($post['subject']);
+    $post['username'] = $db->escape_string($post['username']);
+    $post['message'] = $db->escape_string($post['message']);
+    $post['editreason'] = $db->escape_string($post['editreason']);
+    $post['tags'] = $db->escape_string($post['tags']);
+    
+    return $post;
+}
+
+$plugins->add_hook('admin_tools_permissions', 'trashbin_admin_tools_permissions');
+function trashbin_admin_tools_permissions(&$admin_permissions)
+{
+    $admin_permissions['trashbin'] = "Can manage trash bin?";
+}
+
+$plugins->add_hook('admin_config_action_handler', 'trashbin_admin_config_action_handler');
+function trashbin_admin_config_action_handler(&$actions)
+{
+    $actions['trashbin'] = array(
+        'active' => 'trashbin',
+        'file' => 'trashbin.php',
+    );
+}
+
+class trashbin
+{
+    public function build_url($params = array())
+    {
+        global $PL;
+        $PL or require_once PLUGINLIBRARY;
+
+        if (defined('IN_ADMINCP'))
+        {
+            $url = 'index.php?module=tools-trashbin';
+        }
+        else
+        {
+            $url = 'modcp.php?action=trashbin';
+        }
+
+        if (!is_array($params))
+        {
+            $params = explode('=', $params);
+            if (isset($params[0]) && isset($params[1]))
+            {
+                $params = array($params[0] => $params[1]);
+            }
+            else
+            {
+                $params = array();
+            }
+        }
+
+        return $PL->url_append($url, $params);
+    }
+
+    function admin_redirect($message = '', $error = false, $action = "")
+    {
+        if ($message)
+        {
+            flash_message($message, ($error ? 'error' : 'success'));
+        }
+
+        if($action != ""){
+            $parm = array("action" => $action);
+        }else{
+            $parm = array();
+        }
+
+        admin_redirect($this->build_url($parm));
+        exit;
+    }
+
+}
+
+?>
