bbb91d4063051fd608018f039817d9fbe7900c9c
Stefan Lenders <stefan@lenders-it.nl>
2017-03-20 09:46:32 +0100

    Dutch Language file created

---
+++ inc/languages/dutch/admin/tools_trashbin.lang.php
@@ -0,0 +1,45 @@
+<?php
+
+$l['trashbin'] = 'Prullenbak';
+$l['trashbin_desc'] = 'Verplaatst alle definitief verwijderde discussies en berichten naar een prullenbak voor 60 dagen';
+$l['trashbin_uninstall'] = "De-installeer prullenbak";
+$l['trashbin_uninstall_question'] = "Weet u zeker dat u de prullenbak plugin wilt verwijderen? Hiermee worden ook alle onderdelen in de prullenbak verwijderd.";
+
+$l['trashbin_restore_thread_removed'] = "De discussie bestaat niet meer!";
+$l['trashbin_restore_no_post'] = "Het bericht dat u probeert te herstellen kan niet worden gevonden!";
+$l['trashbin_restore_forum_removed'] = "Het forum bestaat niet meer!";
+$l['trashbin_restore_no_thread'] = "De discussie die u probeert te herstellen kan niet worden gevonden!";
+$l['trashbin_thread_restored'] = "De geselecteerde discussie is hersteld.";
+$l['trashbin_post_restored'] = "Het geselecteerde bericht is hersteld.";
+$l['trashbin_wrong'] = "Oops er is iets fout gegaan";
+
+$l['trashbin_admin_permission'] = "Mag de prullenbak beheren?";
+
+$l['trashbin_thread_bin'] = 'Discussie prullenbak';
+$l['trashbin_thread_bin_desc'] = 'Bekijk alle verwijderde discussies';
+$l['trashbin_post_bin'] = 'Berichten prullenbak';
+$l['trashbin_post_bin_desc'] = 'Bekijk alle verwijderde berichten';
+
+$l['trashbin_restore_thread'] = 'Herstel discussie';
+$l['trashbin_restore_post'] = 'Herstel bericht';
+$l['trashbin_view_thread'] = 'Bekijk discussie';
+$l['trashbin_view_thread_desc'] = 'Bekijk discussie met alle berichten';
+$l['trashbin_view_post'] = 'Bekijk berichten';
+$l['trashbin_view_post_desc'] = 'Bekijk een enkel bericht';
+
+$l['trashbin_thread_subject'] = 'Discussie onderwerp';
+$l['trashbin_post_subject'] = 'Bericht onderwerp';
+$l['trashbin_poster'] = 'Schrijver';
+$l['trashbin_deleted_by'] = 'Verwijderd door';
+$l['trashbin_deleted_on'] = 'Verwijderd op';
+$l['trashbin_empty'] = 'De prullenbak is leeg';
+$l['trashbin_removed_thread'] = 'VERWIJDERDE DISCUSSIE';
+$l['trashbin_view'] = 'Bekijk';
+$l['trashbin_restore'] = 'Herstel';
+$l['trashbin_posts'] = "Berichten";
+
+$l['trashbin_no_posts'] = 'Deze discussie bevat geen berichten!';
+$l['trashbin_no_thread_exists'] = 'De discussie die u probeert te bekijken bestaat niet!';
+$l['trashbin_no_post_exists'] = 'Het bericht dat u probeert te bekijken bestaat niet!';
+$l['trashbin_edited'] = 'Het bericht was aangepast op:';
+$l['trashbin_by'] = 'door';
